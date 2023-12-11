f42ce956e558d3f0079556033d53ab2c20cca60b
Felipe Pinheiro <felipe.pinheiro.90@hotmail.com>
2023-12-11 19:22:38 -0300

    Day 1: Trebuchet?! (Part Two)

--- src/01/index.ts
+++ src/01/index.ts
@@ -2,16 +2,80 @@ import path from "node:path";
 
 import { readFileLineByLine } from "../utils/read-file";
 
-const DIGIT_REGEX = /\d/;
+type LineMatch = {
+  digit: number;
+  index: number;
+};
+
+const CALIBRATION_MATCH_UTILS = [
+  {
+    callback: (value: string) => Number.parseInt(value),
+    createRegex: () => /\d/dg,
+  },
+  {
+    callback: () => 1,
+    createRegex: () => /one/dg,
+  },
+  {
+    callback: () => 2,
+    createRegex: () => /two/dg,
+  },
+  {
+    callback: () => 3,
+    createRegex: () => /three/dg,
+  },
+  {
+    callback: () => 4,
+    createRegex: () => /four/dg,
+  },
+  {
+    callback: () => 5,
+    createRegex: () => /five/dg,
+  },
+  {
+    callback: () => 6,
+    createRegex: () => /six/dg,
+  },
+  {
+    callback: () => 7,
+    createRegex: () => /seven/dg,
+  },
+  {
+    callback: () => 8,
+    createRegex: () => /eight/dg,
+  },
+  {
+    callback: () => 9,
+    createRegex: () => /nine/dg,
+  },
+];
 
 const getLineCalibrationValue = (line: string) => {
-  const digits = [...line].filter((char) => DIGIT_REGEX.test(char));
+  const matches = CALIBRATION_MATCH_UTILS.reduce<LineMatch[]>(
+    (matches, util) => {
+      const regex = util.createRegex();
 
-  if (digits.length === 0) {
-    throw new Error(`Line ${line} don't have any digits`);
-  }
+      let match = null;
 
-  return Number.parseInt(`${digits[0]}${digits[digits.length - 1]}`);
+      do {
+        match = regex.exec(line);
+
+        if (match !== null) {
+          matches.push({
+            digit: util.callback(match[0]),
+            index: match.index,
+          });
+        }
+      } while (match !== null);
+
+      return matches;
+    },
+    [] as LineMatch[],
+  );
+
+  matches.sort((a, b) => a.index - b.index);
+
+  return matches[0].digit * 10 + matches[matches.length - 1].digit;
 };
 
 (async () => {
